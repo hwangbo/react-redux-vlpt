ec933f056322c74399c1abdf298e5cd6ed9c5a16
hwangbo <taekwonod714@naver.com>
2020-06-30 17:33:52 +0900

    feat: 리덕스 모듈 만들기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './exercise';
+import {createStore} from "redux";
+import {Provider} from 'react-redux';
+import rootReducer from "./modules";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
