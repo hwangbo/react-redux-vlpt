abc00235d99e4360503e799cb2478592236a9f77
hwangbo <taekwonod714@naver.com>
2020-06-30 18:07:56 +0900

    feat: 투두리스트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 }
